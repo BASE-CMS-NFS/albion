81921c6a5f2a0c7e5841631d03899657003b9d4a
niko <nikofigit@gmail.com>
2023-06-21 11:24:42 +0700

    oke

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,9 +44,9 @@
           <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto border-bottom border-lg-bottom-0 pt-2 pt-lg-0">
-              <li class="nav-item"><a class="nav-link active active" aria-current="page" href="#">About</a></li>
-              <li class="nav-item"><a class="nav-link" href="#">Features</a></li>
-              <li class="nav-item"><a class="nav-link" href="#">Help </a></li>
+              <li class="nav-item"><a class="nav-link active active" aria-current="page" href="#">Home</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Statistik</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Features </a></li>
             </ul>
             <form class="d-flex py-3 py-lg-0">
               <a class="btn btn-link text-1000 fw-medium order-1 order-lg-0" href="{{url('login')}}">Sign in</a>
